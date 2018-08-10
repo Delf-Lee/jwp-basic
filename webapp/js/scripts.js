e14524c5583cc809f94ed2bdef4fe30d52e1c33a
Delf-Lee <ttjs2008@naver.com>
2018-08-11 01:00:39 +0900

    [#2] Fix add answer feature

--- webapp/js/scripts.js
+++ webapp/js/scripts.js
@@ -7,16 +7,16 @@ function addAnswer(e) {
     var queryString = $("form[name=answer]").serialize();
 
     $.ajax({
-        type: 'post',
-        url: '/api/qna/addAnswer',
-        data: queryString,
-        dataType: 'json',
+        type : 'post',
+        url : '/api/qna/addAnswer',
+        data : queryString,
+        dataType : 'json',
         error: onError,
-        success: onSuccess,
+        success : onSuccess,
     });
-}
+}ㅇ
 
-function onSuccess(json, status) {
+function onSuccess(json, status){
     var answerTemplate = $("#answerTemplate").html();
     var template = answerTemplate.format(json.writer, new Date(json.createdDate), json.contents, json.answerId, json.answerId);
     $(".qna-comment-slipp-articles").prepend(template);
@@ -50,9 +50,9 @@ function deleteAnswer(e) {
     });
 }
 
-String.prototype.format = function () {
+String.prototype.format = function() {
     var args = arguments;
-    return this.replace(/{(\d+)}/g, function (match, number) {
+    return this.replace(/{(\d+)}/g, function(match, number) {
         return typeof args[number] != 'undefined'
             ? args[number]
             : match
